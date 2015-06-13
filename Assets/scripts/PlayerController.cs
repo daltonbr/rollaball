bd74605cc41f2d2b2a51cd633bc573a80485f12b
daltonbr <sacinopatinete@gmail.com>
2015-06-13 17:41:44 -0300

    FIX: adicionado playerController.cs

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    void FixedUpdate() {
+    
+        float moveHorizontal = Input.GetAxis("Horizontal");
+    		float moveVertical = Input.GetAxis("Vertical");
+    		
+    		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  
+    		
+    		GetComponent<Rigidbody>().AddForce (movement);
+    }
+	
+}
