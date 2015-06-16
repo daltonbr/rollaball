ddf3a6b5695eefba36af794b04ae5d550e1ceb7a
daltonbr <sacinopatinete@gmail.com>
2015-06-16 00:48:59 -0300

    05 - Setting up the play (updated v5.0)

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,16 +2,23 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
-    void FixedUpdate() {
-    
-     		float moveHorizontal = Input.GetAxis("Horizontal");
-    		float moveVertical = Input.GetAxis("Vertical");
-			//Debug.Log ("Horizontal: " + moveHorizontal + "Vertical: " + moveVertical);	
-
-    		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);  
-    		
-    		GetComponent<Rigidbody>().AddForce (movement);
-    }
 	
+	public float speed;
+	
+	private Rigidbody rb;
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce (movement * speed);
+	}
 }
