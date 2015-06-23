de14a02cf497ecde83a073b47bd23c6aaee6b6ee
daltonbr <sacinopatinete@gmail.com>
2015-06-23 17:22:51 -0300

    [1.0] finished all the Unity tutorial

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	
 	public float speed;
+    public Text countText;
+    public Text winText;
 	
 	private Rigidbody rb;
+    private int count;
 	
 	void Start ()
 	{
+        count = 0;
 		rb = GetComponent<Rigidbody>();
-	}
+        SetCountText();  //atualiza a contagem de pontos no UI
+        winText.text = "";
+    }
 	
 	void FixedUpdate ()
 	{
@@ -27,6 +34,18 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag("Pick Up") )
 		{
 		    other.gameObject.SetActive(false);
-		}
+            count++;
+            SetCountText();
+        }
 	}
+
+    void SetCountText()   //atualiza a contagem de pontos no UI
+    {
+        countText.text = "Count: " + count.ToString();
+        if ( count >= 12 )
+        {
+            winText.text = "You Win!";
+        }
+    }
+
 }
