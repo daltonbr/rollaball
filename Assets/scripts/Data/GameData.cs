60e82da428ad2857f9b7c8025d4f819c502cfb84
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-08-12 21:30:34 -0300

    Adds ranking-related classes

---
+++ Assets/scripts/Data/GameData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants {
+	public class Tags {
+		public const string PickUps = "Pick Up";
+		public const string SC = "Status Change";
+		public const string GController = "GameController";
+	}
+
+	public const string MainMenu = "MainMenu";
+	public const string LevelSelect = "LevelSelector";
+	public const string Shop = "Shop";
+
+	public enum Levels {
+		Level01 = 0,
+		Level02 = 1,
+		Level03 = 2,
+
+		Max // Deve sempre ser o ultimo
+	}
+}
+
+public class RankingData
+{
+	public const int MaxEntries = 10;
+	
+	public class Rank {
+		public string[] Name = new string[MaxEntries];
+		public float[] Score = new float[MaxEntries];
+	}
+	
+	public Rank[] Ranks { get; set; }
+
+	public RankingData()
+	{
+		this.Ranks = new Rank[(int)IORanking.Ranking.Max];
+		for (int i = 0; i < (int)IORanking.Ranking.Max; i++)
+		{
+			this.Ranks[i] = new Rank();
+		}
+	}
+}
---
+++ Assets/scripts/Utils/StringTools.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Collections;
+
+// Source: http://www.dotnetperls.com/truncate
+
+/// <summary>
+/// Custom string utility methods.
+/// </summary>
+public static class StringTool
+{
+	/// <summary>
+	/// Get a substring of the first N characters.
+	/// </summary>
+	public static string Truncate(string source, int length)
+	{
+		if (source.Length > length)
+		{
+			source = source.Substring(0, length);
+		}
+		return source;
+	}
+
+	public static string GetString(byte[] bytes)
+	{
+		int count = 0;
+		for (int i = 0; i < bytes.Length; i++)
+		{
+			if (bytes[i] == 0x00)
+				break;
+
+			count++;
+		}
+
+		byte[] str = new byte[count];
+
+		for (int i = 0; i < count; i++)
+		{
+			if (bytes[i] != 0x00)
+				str[i] = bytes[i];
+			else
+				break;
+		}
+
+		if (count <= 0)
+			return "";
+
+		return Encoding.ASCII.GetString (str);
+	}
+}
