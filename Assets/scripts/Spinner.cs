c9dc97552066b5e9153e97801d215d1f1963a973
daltonbr <sacinopatinete@gmail.com>
2015-08-18 11:41:57 -0300

    Added Die and Boundary, and Spinning Cannon

---
+++ Assets/scripts/Spinner.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Spinner : MonoBehaviour
+{
+
+    public float angularVelocity = 1;
+
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * angularVelocity);
+    }
+}
