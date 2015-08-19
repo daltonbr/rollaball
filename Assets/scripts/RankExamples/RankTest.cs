7ba75a97d75f6f8cd6b723d4d618a70407b79d16
Guilherme G. M <guilherme.menaldo@outlook.com>
2015-08-19 18:55:07 -0300

    Adicionado prefab para exibir o ranking Adicionado exemplos para o uso do sistema de ranking - cenas: RankingDisplay e RankingTest - scripts: RankTest; RankDisplay

---
+++ Assets/scripts/RankExamples/RankTest.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RankTest : MonoBehaviour {
+	public InputField nome;
+	public int score = 0;
+
+	void Awake()
+	{
+		// Isso deve ser feito no menu principal
+		// para conferir a existencia dos arquivos
+		IORanking.Init ();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		IORanking.UpdateRank(Constants.Levels.Level01, nome.text, 0f, score);
+	}
+}
---
+++ Assets/scripts/RankExamples/RankingDisplay.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RankingDisplay : MonoBehaviour {
+
+	// Armazena todos os rankings de todas as fases
+	RankingData[] rd;
+
+	public Transform displayGroup;
+	public GameObject rankEntry;
+
+	// Use this for initialization
+	void Start () {
+		// Deve ser chamado no inicio do menu principal
+		// para confirmar a exitencia de arquivos
+		IORanking.Init ();
+
+		rd = IORanking.Load ();
+		ShowRank ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowRank()
+	{
+		//rd[level].Ranks[RankId]
+		//for (int i = 0; i < IORanking.Ranking.Max; i++)
+		//{
+		RankingData.Rank rank = rd[(int)Constants.Levels.Level01].Ranks[(int)IORanking.Ranking.BestScore];
+		for (int i = 0; i < RankingData.MaxEntries; i++) {
+			GameObject rentry = Instantiate (rankEntry) as GameObject;
+			RankEntryObject ro = rentry.GetComponent<RankEntryObject> ();
+			ro.Pos.text = ""+(i+1);
+			ro.Name.text = rank.Name[i];
+			ro.Score.text = rank.Score[i].ToString();
+			rentry.transform.SetParent(displayGroup);
+		}
+		//}
+	}
+}
